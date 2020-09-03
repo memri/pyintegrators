45d50b4400fbf95e465dfa08be141c155d5d8c20
Koen van der Veen <koenlennartvanderveen@gmail.com>
2020-09-03 18:08:01 +0200

    --

--- tools/test_in_ci.sh
+++ tools/test_in_ci.sh
@@ -3,7 +3,7 @@
 curl https://gitlab.memri.io/memri/pod/uploads/83961119f78969e6accdd0453f4dfbc2/pod-from-docker-v0.2.0-9-ga68dee2 -o pod_docker
 
 RUST_LOG=pod=debug,info \
-  ./pod_docker \
+  ./tools/pod_docker \
   --owners=ANY \
   --insecure-non-tls=0.0.0.0 \
   --insecure-http-headers \
