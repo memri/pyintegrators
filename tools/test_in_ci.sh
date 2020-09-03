cc0bf4087c32a60a3d75a5bc20e850003929f600
Koen van der Veen <koenlennartvanderveen@gmail.com>
2020-09-03 17:56:41 +0200

    Ci fix

--- tools/test_in_ci.sh
+++ tools/test_in_ci.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
 curl https://gitlab.memri.io/memri/pod/uploads/83961119f78969e6accdd0453f4dfbc2/pod-from-docker-v0.2.0-9-ga68dee2 -o pod_docker
-./pod_docker & 
+
+RUST_LOG=pod=debug,info \
+  ./pod_docker \
+  --owners=ANY \
+  --insecure-non-tls=0.0.0.0 \
+  --insecure-http-headers \
+  "$@" &
+
 pid=$$
 nbdev_test_nbs
 kill $pid
